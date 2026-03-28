d30ef9f9bb94bf1e454f14a760a1c25393f4daa4
draviralchaudhary <draviralchaudhary@gmail.com>
2026-03-28 08:13:01 +0530

    update mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -30,4 +30,8 @@ app.get("/orders", async (req, res) => {
   res.send(orders);
 });
 
-app.listen(5000, () => console.log("Backend running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
