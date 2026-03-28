e685511e4046397ca95513030f9b56be8dd043e3
draviralchaudhary <draviralchaudhary@gmail.com>
2026-03-28 10:58:40 +0530

    deploy frontend on railway

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
 
 // 🔥 ROOT ROUTE (IMPORTANT FOR RAILWAY)
 const path = require("path");
