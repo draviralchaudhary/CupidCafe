b695e579487d7b730fa917a2bd737606e5876233
Sagar verma <sagarverma@Sagars-MacBook-Air.local>
2026-03-30 04:25:36 +0530

    frontend ready

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: "/", // 🔥 THIS FIXES YOUR ISSUE
   plugins: [
-    react(), // ✅ Only required plugin
+    react(),
   ],
   resolve: {
     alias: {
